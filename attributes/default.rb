2ce00937e02152714f764674f08a1e712e940890
Yana <yana.didun@yola.com>
2026-02-26 17:30:54 +0200

    Adjust reload-vcl path

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,12 +12,10 @@ default['yum']['epel']['exclude'] = 'varnish' unless node['varnish']['configure'
 default['varnish']['conf_path'] = platform_family?('debian') ? '/etc/default/varnish' : '/etc/sysconfig/varnish'
 
 default['varnish']['reload_cmd'] =
-  if node['varnish']['major_version'] >= 7
-    '/usr/sbin/varnishreload'
+  if node['varnish']['major_version'] >= 6
+    '/usr/share/varnish/reload-vcl'
   elsif node['varnish']['major_version'] < 4
     '/usr/bin/varnish_reload_vcl'
-  elsif platform_family?('debian')
-    '/usr/share/varnish/reload-vcl'
   else
     '/usr/sbin/varnish_reload_vcl'
   end
